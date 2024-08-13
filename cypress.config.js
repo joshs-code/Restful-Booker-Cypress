705e1cdfc3a49891abd9c250ec8bf2fb66d1f616
joshalmasin88 <joshalmasin88@gmail.com>
2024-08-13 07:18:33 -0500

    Updated url with baseurl in config, Utlized fixtures folder to store mock data, Added tests. Utlized json data in seprate file so not hardcoded into script.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://restful-booker.herokuapp.com/',
+    userName: 'Joshua',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/API-Test.cy.js
+++ cypress/e2e/API-Test.cy.js
@@ -1,53 +1,81 @@
-let token, bookingID;
+import bookinginfo from "./test-data/createdBooking.json";
+import updatedinfo from "./test-data/updatedBooking.json";
+import userdata from '../fixtures/userdata.json';
 
 describe("API Testing", () => {
   before("Testing Token", () => {
-    cy.request("POST", "https://restful-booker.herokuapp.com/auth", {
-      username: "admin",
-      password: "password123",
+    cy.request("POST", "auth", {
+      username: userdata.name,
+      password: userdata.password,
     }).then((response) => {
-      token = response.body.token;
+      // Saving Token in Cypress Env Variable instead of global variable
+      Cypress.env("token", response.body.token);
     });
   });
 
   it("Should be able to create a booking", () => {
     cy.request({
       method: "POST",
-      url: "https://restful-booker.herokuapp.com/booking",
+      url: "booking",
       headers: {
         "Content-Type": "application/json",
-        // "Cookie": `token=${token}`
+        Cookie: `token=${Cypress.env("token")}`,
       },
-      body: {
-        firstname: "Joshua",
-        lastname: "Brown",
-        totalprice: 111,
-        depositpaid: true,
-        bookingdates: {
-          checkin: "2018-01-01",
-          checkout: "2019-01-01",
-        },
-        additionalneeds: "Breakfast",
-      },
+      // Passing booking info json instead of hardcode json
+      body: bookinginfo,
     }).then((response) => {
       expect(response.status).eq(200);
       expect(response.body.booking.firstname).to.have.string("Joshua");
-      bookingID = response.body.bookingid;
+      Cypress.env("bookingID", response.body.bookingid);
+    });
+  });
+
+  it("Should be able to get new booking", () => {
+    cy.request({
+      method: "GET",
+      url: `booking/${Cypress.env("bookingID")}`,
+    }).then((response) => {
+      expect(response.body.firstname).to.equal(bookinginfo.firstname);
+      expect(response.body.bookingdates.checkin).to.equal(
+        bookinginfo.bookingdates.checkin
+      );
+    });
+  });
+
+  it("Should be able to update booking", () => {
+    cy.request({
+      method: "PUT",
+      url: `booking/${Cypress.env("bookingID")}`,
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `token=${Cypress.env("token")}`,
+      },
+      body: updatedinfo,
+    }).then((response) => {
+      expect(response.body.firstname).to.eq(updatedinfo.firstname);
     });
   });
 
   it("Should be able to delete new booking", () => {
-
     cy.request({
-      method: 'DELETE',
-      url: `https://restful-booker.herokuapp.com/booking/${bookingID}`,
+      method: "DELETE",
+      url: `booking/${Cypress.env("bookingID")}`,
       headers: {
-        'Cookie': `token=${token}`
-      }
-    }).then( (response) => {
+        Cookie: `token=${Cypress.env("token")}`,
+      },
+    }).then((response) => {
+      // Expect 204 Status for deleted
       expect(response.status).eq(204);
-    })
-
+    });
   });
 
+  it("Should make sure booking is deleted", () => {
+    cy.request({
+      method: "GET",
+      url: `booking/${Cypress.env("bookingID")}`,
+      failOnStatusCode: false,
+    }).as("booking");
+
+    cy.get("@booking").its("status").should("eq", 404);
+  });
 });
